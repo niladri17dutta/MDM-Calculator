4e693038d8a82748d886f8857fd4141f5c291220
niladri17dutta <niladridutta.2005@gmail.com>
2024-06-16 12:07:00 +0530

    Formatted spacing in script1-5.js & script6-8.js

--- script1-5.js
+++ script1-5.js
@@ -7,201 +7,210 @@ document.getElementById('calculateButton').addEventListener('click', function() 
   let valueday = document.getElementById('userInputDay').value;
 
 
-    // No Change START they will remain constant
-    let rice = parseInt(valuestudent) / 10;
-    quanrice.value = rice.toFixed(3);
-    // No Change OVER
+  // No Change START they will remain constant
+  let rice = parseInt(valuestudent) / 10;
+  quanrice.value = rice.toFixed(3);
+  // No Change OVER
+  
+  if (valuestudent == '550'){
+    if (valueday==='Monday') {
 
-      if (valueday==='Monday') {
+      let dalqt = ((parseInt(valuestudent) * 20) / 1000);
+      quandal.value = dalqt.toFixed(3);
+      let dalpr = dalqt * 90;
+      pricdal.value = dalpr.toFixed(2);
 
-        let dalqt = ((parseInt(valuestudent) * 20) / 1000);
-        quandal.value = dalqt.toFixed(3);
-        let dalpr = dalqt * 90;
-        pricdal.value = dalpr.toFixed(2);
+      let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
+      quansabji.value = sabjiqt.toFixed(3);
+      let sabjipr = sabjiqt * 22;
+      pricsabji.value = sabjipr.toFixed(2);
 
-        let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
-        quansabji.value = sabjiqt.toFixed(3);
-        let sabjipr = sabjiqt * 22;
-        pricsabji.value = sabjipr.toFixed(2);
+      let oilqt = ((parseInt(valuestudent) * 5) / 1000);
+      quanoil.value = oilqt.toFixed(3);
+      let oilpr = oilqt * 180;
+      pricoil.value = oilpr.toFixed(2);
 
-        let oilqt = ((parseInt(valuestudent) * 5) / 1000);
-        quanoil.value = oilqt.toFixed(3);
-        let oilpr = oilqt * 180;
-        pricoil.value = oilpr.toFixed(2);
+      let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
+      quanjalawan.value = jalawanqt.toFixed(3);
+      let jalawanpr = jalawanqt* 12;
+      pricjalawan.value = jalawanpr.toFixed(2);
 
-        let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
-        quanjalawan.value = jalawanqt.toFixed(3);
-        let jalawanpr = jalawanqt* 12;
-        pricjalawan.value = jalawanpr.toFixed(2);
+      // No Change START they will remain constant
+      let totalamt  = parseInt(valuestudent)*5.45;
+      totamt.value = totalamt.toFixed(2);
 
-        // No Change START they will remain constant
-        let totalamt  = parseInt(valuestudent)*5.45;
-        totamt.value = totalamt.toFixed(2);
-
-        let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
-        pricsalt.value = saltpr.toFixed(2);
-        // No Change OVER
+      let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
+      pricsalt.value = saltpr.toFixed(2);
+      // No Change OVER
           
-      }
+    }
 
-      else if (valueday==='Tuesday'){
+    else if (valueday==='Tuesday'){
 
-        let dalqt = ((parseInt(valuestudent) * 20) / 1000);
-        quandal.value = dalqt.toFixed(3);
-        let dalpr = dalqt * 90;
-        pricdal.value = dalpr.toFixed(2);
+      let dalqt = ((parseInt(valuestudent) * 20) / 1000);
+      quandal.value = dalqt.toFixed(3);
+      let dalpr = dalqt * 90;
+      pricdal.value = dalpr.toFixed(2);
 
-        let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
-        quansabji.value = sabjiqt.toFixed(3);
-        let sabjipr = sabjiqt * 22;
-        pricsabji.value = sabjipr.toFixed(2);
+      let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
+      quansabji.value = sabjiqt.toFixed(3);
+      let sabjipr = sabjiqt * 22;
+      pricsabji.value = sabjipr.toFixed(2);
 
-        let oilqt = ((parseInt(valuestudent) * 5) / 1000);
-        quanoil.value = oilqt.toFixed(3);
-        let oilpr = oilqt * 180;
-        pricoil.value = oilpr.toFixed(2);
+      let oilqt = ((parseInt(valuestudent) * 5) / 1000);
+      quanoil.value = oilqt.toFixed(3);
+      let oilpr = oilqt * 180;
+      pricoil.value = oilpr.toFixed(2);
 
-        let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
-        quanjalawan.value = jalawanqt.toFixed(3);
-        let jalawanpr = jalawanqt* 12;
-        pricjalawan.value = jalawanpr.toFixed(2);
+      let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
+      quanjalawan.value = jalawanqt.toFixed(3);
+      let jalawanpr = jalawanqt* 12;
+      pricjalawan.value = jalawanpr.toFixed(2);
 
-        // No Change START they will remain constant
-        let totalamt  = parseInt(valuestudent)*5.45;
-        totamt.value = totalamt.toFixed(2);
+      // No Change START they will remain constant
+      let totalamt  = parseInt(valuestudent)*5.45;
+      totamt.value = totalamt.toFixed(2);
 
-        let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
-        pricsalt.value = saltpr.toFixed(2);
-        // No Change OVER
+      let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
+      pricsalt.value = saltpr.toFixed(2);
+      // No Change OVER
         
-      }
+    }
 
-      else if (valueday==='Wednesday'){
+    else if (valueday==='Wednesday'){
 
-        let dalqt = ((parseInt(valuestudent) * 20) / 1000);
-        quandal.value = dalqt.toFixed(3);
-        let dalpr = dalqt * 90;
-        pricdal.value = dalpr.toFixed(2);
+      let dalqt = ((parseInt(valuestudent) * 20) / 1000);
+      quandal.value = dalqt.toFixed(3);
+      let dalpr = dalqt * 90;
+      pricdal.value = dalpr.toFixed(2);
 
-        let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
-        quansabji.value = sabjiqt.toFixed(3);
-        let sabjipr = sabjiqt * 22;
-        pricsabji.value = sabjipr.toFixed(2);
+      let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
+      quansabji.value = sabjiqt.toFixed(3);
+      let sabjipr = sabjiqt * 22;
+      pricsabji.value = sabjipr.toFixed(2);
 
-        let oilqt = ((parseInt(valuestudent) * 5) / 1000);
-        quanoil.value = oilqt.toFixed(3);
-        let oilpr = oilqt * 180;
-        pricoil.value = oilpr.toFixed(2);
+      let oilqt = ((parseInt(valuestudent) * 5) / 1000);
+      quanoil.value = oilqt.toFixed(3);
+      let oilpr = oilqt * 180;
+      pricoil.value = oilpr.toFixed(2);
 
-        let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
-        quanjalawan.value = jalawanqt.toFixed(3);
-        let jalawanpr = jalawanqt* 12;
-        pricjalawan.value = jalawanpr.toFixed(2);
+      let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
+      quanjalawan.value = jalawanqt.toFixed(3);
+      let jalawanpr = jalawanqt* 12;
+      pricjalawan.value = jalawanpr.toFixed(2);
 
-        // No Change START they will remain constant
-        let totalamt  = parseInt(valuestudent)*5.45;
-        totamt.value = totalamt.toFixed(2);
+      // No Change START they will remain constant
+      let totalamt  = parseInt(valuestudent)*5.45;
+      totamt.value = totalamt.toFixed(2);
 
-        let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
-        pricsalt.value = saltpr.toFixed(2);
-        // No Change OVER
+      let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
+      pricsalt.value = saltpr.toFixed(2);
+      // No Change OVER
         
-      }
+    }
 
-      else if (valueday==='Thursday'){
+    else if (valueday==='Thursday'){
 
-        let dalqt = ((parseInt(valuestudent) * 20) / 1000);
-        quandal.value = dalqt.toFixed(3);
-        let dalpr = dalqt * 90;
-        pricdal.value = dalpr.toFixed(2);
+      let dalqt = ((parseInt(valuestudent) * 20) / 1000);
+      quandal.value = dalqt.toFixed(3);
+      let dalpr = dalqt * 90;
+      pricdal.value = dalpr.toFixed(2);
 
-        let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
-        quansabji.value = sabjiqt.toFixed(3);
-        let sabjipr = sabjiqt * 22;
-        pricsabji.value = sabjipr.toFixed(2);
+      let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
+      quansabji.value = sabjiqt.toFixed(3);
+      let sabjipr = sabjiqt * 22;
+      pricsabji.value = sabjipr.toFixed(2);
 
-        let oilqt = ((parseInt(valuestudent) * 5) / 1000);
-        quanoil.value = oilqt.toFixed(3);
-        let oilpr = oilqt * 180;
-        pricoil.value = oilpr.toFixed(2);
+      let oilqt = ((parseInt(valuestudent) * 5) / 1000);
+      quanoil.value = oilqt.toFixed(3);
+      let oilpr = oilqt * 180;
+      pricoil.value = oilpr.toFixed(2);
 
-        let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
-        quanjalawan.value = jalawanqt.toFixed(3);
-        let jalawanpr = jalawanqt* 12;
-        pricjalawan.value = jalawanpr.toFixed(2);
+      let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
+      quanjalawan.value = jalawanqt.toFixed(3);
+      let jalawanpr = jalawanqt* 12;
+      pricjalawan.value = jalawanpr.toFixed(2);
 
-        // No Change START they will remain constant
-        let totalamt  = parseInt(valuestudent)*5.45;
-        totamt.value = totalamt.toFixed(2);
+      // No Change START they will remain constant
+      let totalamt  = parseInt(valuestudent)*5.45;
+      totamt.value = totalamt.toFixed(2);
 
-        let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
-        pricsalt.value = saltpr.toFixed(2);
-        // No Change OVER
+      let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
+      pricsalt.value = saltpr.toFixed(2);
+      // No Change OVER
 
-      }
+    }
 
-      else if (valueday==='Friday'){
+    else if (valueday==='Friday'){
 
-        let dalqt = ((parseInt(valuestudent) * 20) / 1000);
-        quandal.value = dalqt.toFixed(3);
-        let dalpr = dalqt * 90;
-        pricdal.value = dalpr.toFixed(2);
+      let dalqt = ((parseInt(valuestudent) * 20) / 1000);
+      quandal.value = dalqt.toFixed(3);
+      let dalpr = dalqt * 90;
+      pricdal.value = dalpr.toFixed(2);
 
-        let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
-        quansabji.value = sabjiqt.toFixed(3);
-        let sabjipr = sabjiqt * 22;
-        pricsabji.value = sabjipr.toFixed(2);
+      let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
+      quansabji.value = sabjiqt.toFixed(3);
+      let sabjipr = sabjiqt * 22;
+      pricsabji.value = sabjipr.toFixed(2);
 
-        let oilqt = ((parseInt(valuestudent) * 5) / 1000);
-        quanoil.value = oilqt.toFixed(3);
-        let oilpr = oilqt * 180;
-        pricoil.value = oilpr.toFixed(2);
+      let oilqt = ((parseInt(valuestudent) * 5) / 1000);
+      quanoil.value = oilqt.toFixed(3);
+      let oilpr = oilqt * 180;
+      pricoil.value = oilpr.toFixed(2);
 
-        let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
-        quanjalawan.value = jalawanqt.toFixed(3);
-        let jalawanpr = jalawanqt* 12;
-        pricjalawan.value = jalawanpr.toFixed(2);
+      let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
+      quanjalawan.value = jalawanqt.toFixed(3);
+      let jalawanpr = jalawanqt* 12;
+      pricjalawan.value = jalawanpr.toFixed(2);
 
-        // No Change START they will remain constant
-        let totalamt  = parseInt(valuestudent)*5.45;
-        totamt.value = totalamt.toFixed(2);
+      // No Change START they will remain constant
+      let totalamt  = parseInt(valuestudent)*5.45;
+      totamt.value = totalamt.toFixed(2);
 
-        let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
-        pricsalt.value = saltpr.toFixed(2);
-        // No Change OVER
+      let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
+      pricsalt.value = saltpr.toFixed(2);
+      // No Change OVER
 
-      }
-      else if (valueday==='Saturday'){
+    }
+    
+    else if (valueday==='Saturday'){
 
-        let dalqt = ((parseInt(valuestudent) * 20) / 1000);
-        quandal.value = dalqt.toFixed(3);
-        let dalpr = dalqt * 90;
-        pricdal.value = dalpr.toFixed(2);
+      let dalqt = ((parseInt(valuestudent) * 20) / 1000);
+      quandal.value = dalqt.toFixed(3);
+      let dalpr = dalqt * 90;
+      pricdal.value = dalpr.toFixed(2);
 
-        let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
-        quansabji.value = sabjiqt.toFixed(3);
-        let sabjipr = sabjiqt * 22;
-        pricsabji.value = sabjipr.toFixed(2);
+      let sabjiqt = ((parseInt(valuestudent) * 50) / 1000);
+      quansabji.value = sabjiqt.toFixed(3);
+      let sabjipr = sabjiqt * 22;
+      pricsabji.value = sabjipr.toFixed(2);
 
-        let oilqt = ((parseInt(valuestudent) * 5) / 1000);
-        quanoil.value = oilqt.toFixed(3);
-        let oilpr = oilqt * 180;
-        pricoil.value = oilpr.toFixed(2);
+      let oilqt = ((parseInt(valuestudent) * 5) / 1000);
+      quanoil.value = oilqt.toFixed(3);
+      let oilpr = oilqt * 180;
+      pricoil.value = oilpr.toFixed(2);
 
-        let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
-        quanjalawan.value = jalawanqt.toFixed(3);
-        let jalawanpr = jalawanqt* 12;
-        pricjalawan.value = jalawanpr.toFixed(2);
+      let jalawanqt = ((parseInt(valuestudent) * 100) / 1000);
+      quanjalawan.value = jalawanqt.toFixed(3);
+      let jalawanpr = jalawanqt* 12;
+      pricjalawan.value = jalawanpr.toFixed(2);
 
-        // No Change START they will remain constant
-        let totalamt  = parseInt(valuestudent)*5.45;
-        totamt.value = totalamt.toFixed(2);
+      // No Change START they will remain constant
+      let totalamt  = parseInt(valuestudent)*5.45;
+      totamt.value = totalamt.toFixed(2);
 
-        let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
-        pricsalt.value = saltpr.toFixed(2);
-        // No Change OVER
+      let saltpr = totalamt - (dalpr+sabjipr+oilpr+jalawanpr);
+      pricsalt.value = saltpr.toFixed(2);
+      // No Change OVER
 
-      }
-      else alert('Enter Corrct Input Values!');
+    }
+    else {
+      alert('Enter Corrct Input Values!');
+    }
+
+  }
+  else {
+    alert('Enter Corrct Input Values!');
+  }
 
 });
